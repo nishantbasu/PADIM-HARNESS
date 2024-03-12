4ef4cbc658e4d3140d0a75706833791fac057f59
nishantbasu <nishantbasu100@gmail.com>
2024-03-12 17:51:59 +0530

    some changes

--- controller/padim.js
+++ controller/padim.js
@@ -1,7 +1,37 @@
 const {StatusCodes} = require ('http-status-codes');
+const { spawn } = require('child_process');
+const path = require('path');
+
 
 const uploadTrainingImages = async (req, res, next) => {
-    res.status(StatusCodes.OK).send();
+    const { id: orderId } = req.params;
+    const dataPathResolved = path.join(__dirname, `../uploads/${orderId}`);
+    const modelPathResolved = path.join(__dirname, `../uploads/${orderId}`);
+    const UUID = orderId;
+    const args = [
+        '--data_path', dataPathResolved,
+        '--model_path', modelPathResolved,
+        '--UUID', UUID,
+        '--train' // or '--train' depending on your requirements
+    ];
+    const pythonScriptPath = path.join(__dirname, '../ml/ModelManager.py');
+
+    const pythonProcess = spawn('python', [pythonScriptPath, ...args]);
+
+    // Handle stdout, stderr
+    pythonProcess.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+    });
+
+    pythonProcess.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`);
+    });
+
+    // Handle process exit
+    pythonProcess.on('close', (code) => {
+        console.log(`Python process exited with code ${code}`);
+        res.send(`Python process exited with code ${code}`);
+    });
 };
 
 module.exports = { uploadTrainingImages };
